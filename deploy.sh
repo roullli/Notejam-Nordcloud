6f5e6369a9d7de8ac9bb2427fea3fb2e4090081d
roullli <rohollah.ehsani@gmail.com>
2021-09-23 14:36:27 +0300

    Dockerfile update - fixed some issues

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-docker build -t roullli/notejam:latest -t roullli/notejam:$SHA_V .
-docker push roullli/notejam:latest
+docker build -t roullli/notejam:$SHA_V .
 docker push roullli/notejam:$SHA_V
 kubectl apply -f k8s
 kubectl set image deployment/notejam-deployment notejam=roullli/notejam:$SHA_V
